2036aa08475ff8bbf6a549e89a7b121bda598518
ChinJoshi <cshjoshi@gmail.com>
2017-01-03 19:06:52 -0600

    Update TCPClient.cpp

--- TCPClient.cpp
+++ TCPClient.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-	std::string rawIP = "127.0.0.1";
+	std::string rawIP = "192.168.1.252";
 	unsigned short int portNum = 2667;
 	boost::asio::io_service ioService;
 	std::string rawBuffer = "ABCD";
@@ -12,15 +12,18 @@ int main() {
 	boost::asio::ip::address ipAddress = boost::asio::ip::address::from_string(rawIP);
 	boost::asio::ip::tcp::endpoint endpoint(ipAddress, portNum);
 	//Everything below this comment to the next comment is for creating and opening an active socket
-	boost::asio::ip::tcp protocol = boost::asio::ip::tcp::v4();
 	boost::asio::ip::tcp::socket socket(ioService);
-	socket.connect(endpoint,ec);
-	if (ec) {
-		std::cout << ec.message();
+	for (;;) {
+		socket.connect(endpoint, ec);
+		if (ec) {
+			std::cout << ec.message();
+			system("PAUSE");
+			break;
+		}
+		//Everything below this comment to the next comment is for converting the string to a fixed ength buffer that can be used to write to sockets in asio
+		boost::asio::const_buffers_1 finalBuffer = boost::asio::buffer(rawBuffer);
+		//Everything below this comment to the next comment is for writing to the socket
+		boost::asio::write(socket, finalBuffer);
 	}
-	//Everything below this comment to the next comment is for converting the string to a fixed ength buffer that can be used to write to sockets in asio
-	boost::asio::const_buffers_1 finalBuffer = boost::asio::buffer(rawBuffer);
-	//Everything below this comment to the next comment is for writing to the socket
-	boost::asio::write(socket, finalBuffer);
 	return 0;
 }
